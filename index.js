a8ba60bb9f803756a3a005f1c880bfe736ba9f6e
Michael <michael.daniels1013@gmail.com>
2022-01-28 14:43:37 -0600

    Assignment completed.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+
+
+function writeCards(names, eventName) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+        thankYouMessages.push(message);
+    }
+    return thankYouMessages;
+}
+
+
+function countDown() {
+    let countdown = 10 
+
+    while (countdown > -1) {
+
+
+        console.log(countdown--);
+    }
+}
+
+
+
+
